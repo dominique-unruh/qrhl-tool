0a744da11248e106169611a2cfa743242a12794a
Dominique Unruh <unruh@ut.ee>
2017-11-16 19:08:44 +0200

    Added some comments to PrgEnc.thy

--- PrgEnc.thy
+++ PrgEnc.thy
@@ -2,24 +2,33 @@ theory PrgEnc
   imports QRHL_Protocol
 begin
 
+(* Allows us to use "sorry" *)
 declare[[quick_and_dirty]]
-  
+
 typedecl key
 typedecl msg
 
+(* The types of keys and messages must be declared finite, otherwise uniform sampling is not defined *)
 instantiation key :: finite begin instance sorry end
 instantiation msg :: finite begin instance sorry end
+
+(* The type of messages is declared as an additive group,
+   + stands for the bitwise XOR on messages *)
 instantiation msg :: group_add begin instance sorry end
 axiomatization where xor_cancel[simp]: "a+a=0" for a::msg
 
+(* G is an unspecified pseudo-random generator *)
 axiomatization 
   G :: "key \<Rightarrow> msg" 
-  
+
+(* Encryption is defined in terms of G *)
 definition enc :: "key * msg \<Rightarrow> msg"
   where [simp]: "enc = (\<lambda>(k,x). G(k)+x)"
-  
+
+(* Three lemmas that we use in the *.qrhl files *)
 lemma my_simp: "G k1 + x = x + G k1 + m2 + m2"
   by (metis add.assoc add.left_cancel xor_cancel)
+
 lemma mysimp2: "x+m11+m22+m22 = x+m11" for x::msg
   by (metis add.assoc add.left_cancel xor_cancel)
 
@@ -28,7 +37,4 @@ lemma aux_bij: "bij (\<lambda>xb::msg. xb + Gx + xa)"
   apply (metis add.assoc add.left_cancel xor_cancel)
   by (metis add.assoc add.left_cancel xor_cancel)
     
-(* lemma triangle: "\<bar>a - b\<bar> \<le> \<bar>a - c\<bar> + \<bar>b - c\<bar>" for a b c :: real by simp *)
-    
 end
-
