3fb24e2423edcaec4d55f7fac85a9a455a20398d
Dominique Unruh <unruh@ut.ee>
2018-02-05 11:07:12 +0200

    Some minor changes in examples

--- Example.thy
+++ Example.thy
@@ -4,10 +4,8 @@ begin
 
 definition "square x = x*x"
 
-lemma square_simp[simp]: "square x = x*x"
+lemma square_simp[simp]:
+  "square x = x*x"
   using square_def by auto
-
-(* Uncomment this to get detailed type information in the qRHL tool: *)
-(* declare[[show_types,show_sorts]] *)
 
 end
