2d2933ce1ec3413de0b91407c22dca5455f2845c
Dominique Unruh <unruh@ut.ee>
2018-10-26 19:57:53 +0300

    Removed use of "readlink" in proofgeneral.sh, run-isabelle.sh
    Needed for running on MacOS

--- run-isabelle.sh
+++ run-isabelle.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-SELF="$(readlink -f "$0")"
-DIR="$(dirname "$SELF")"
+DIR="$(dirname "$BASH_SOURCE[0]")"
+
+if ! [ -e "$DIR/PG" ]; then
+    echo >/dev/stderr "Cannot determine the installation directory that contains the qrhl tool."
+    echo >/dev/stderr "Please edit the line 'DIR=...' in proofgeneral.sh, it should set DIR to the installation directory."
+    exit 1
+fi
 
 chmod +x "$DIR/bin/qrhl"
 "$DIR"/bin/qrhl --isabelle auto "$@"
