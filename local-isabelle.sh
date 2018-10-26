b56048570a66859b4f8043767ee4e153d8ecc0a6
Dominique Unruh <unruh@ut.ee>
2018-10-27 00:29:33 +0300

    Removed use of readlink from bin/qrhl, local-isabelle.sh

--- local-isabelle.sh
+++ local-isabelle.sh
@@ -2,10 +2,17 @@
 
 # Runs a locally installed Isabelle
 
-set -x
+set -e
 
 ISABELLE_DIR=/opt/Isabelle2018
-DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+DIR="$(dirname "$BASH_SOURCE[0]")"
+
+if ! [ -e "$DIR/isabelle-thys" ]; then
+    echo >/dev/stderr "Cannot determine the installation directory that contains the qrhl tool."
+    echo >/dev/stderr "Please edit the line 'DIR=...' in proofgeneral.sh, it should set DIR to the installation directory."
+    exit 1
+fi
 
 if [ "$#" = 0 ]; then
     FILES=("$DIR/isabelle-thys/Test.thy" "$ISABELLE_DIR/src/Pure/ROOT.ML")
