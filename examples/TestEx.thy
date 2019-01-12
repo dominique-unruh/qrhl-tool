2651b45e46ab5597dd35250f4bff4b7020337457
Dominique Unruh <unruh@ut.ee>
2019-01-12 16:41:10 -0400

    Fix: Rewriting S>>Q = top/bot --> S=top/bot works now (in prepare_for_code)

--- examples/TestEx.thy
+++ examples/TestEx.thy
@@ -2,19 +2,10 @@ theory TestEx
   imports QRHL.QRHL 
 begin
 
-lemma xxx:
+lemma xxxx:
   assumes [simp]: "declared_qvars \<lbrakk>q1,r1,q2,r2\<rbrakk>"
-  shows "(span{ket 0}\<guillemotright>\<lbrakk>q1\<rbrakk>) = top"
+  shows "(\<Sqinter>z::bit*bit. let ebar = mproj computational_basis z\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<cdot> \<top>; fbar = mproj computational_basis z\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<cdot> \<top> in \<CC>\<ll>\<aa>[z = z] \<sqinter> ebar \<sqinter> fbar + ortho ebar + ortho fbar) = Cla[True]"
   apply (simp add: prepare_for_code)
   by eval
-
-(* TODO should work! *)
-lemma xxx:
-  assumes [simp]: "declared_qvars \<lbrakk>q1,r1,q2,r2\<rbrakk>"
-  shows "(\<Sqinter>z. let ebar = mproj computational_basis z\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<cdot> \<top>; fbar = mproj computational_basis z\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<cdot> \<top> in \<CC>\<ll>\<aa>[z = z] \<sqinter> ebar \<sqinter> fbar + ortho ebar + ortho fbar) = Cla[True]"
-  apply (simp add: prepare_for_code)
-  by eval
-
-definition "x == 10"
 
 end
