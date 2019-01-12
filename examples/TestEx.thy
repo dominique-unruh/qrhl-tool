a6ae824be4237878429393604ea23b7aec35eb18
Dominique Unruh <unruh@ut.ee>
2019-01-12 16:21:31 -0400

    Fixed epr.qrhl, added epr-measure.qrhl to match paper. Added comments in .qrhl files that refer to paper.

--- examples/TestEx.thy
+++ examples/TestEx.thy
@@ -1,6 +1,19 @@
 theory TestEx 
-  imports QRHL.QRHL TestEx2
+  imports QRHL.QRHL 
 begin
+
+lemma xxx:
+  assumes [simp]: "declared_qvars \<lbrakk>q1,r1,q2,r2\<rbrakk>"
+  shows "(span{ket 0}\<guillemotright>\<lbrakk>q1\<rbrakk>) = top"
+  apply (simp add: prepare_for_code)
+  by eval
+
+(* TODO should work! *)
+lemma xxx:
+  assumes [simp]: "declared_qvars \<lbrakk>q1,r1,q2,r2\<rbrakk>"
+  shows "(\<Sqinter>z. let ebar = mproj computational_basis z\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<cdot> \<top>; fbar = mproj computational_basis z\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<cdot> \<top> in \<CC>\<ll>\<aa>[z = z] \<sqinter> ebar \<sqinter> fbar + ortho ebar + ortho fbar) = Cla[True]"
+  apply (simp add: prepare_for_code)
+  by eval
 
 definition "x == 10"
 
