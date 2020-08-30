5b74247542aebbd661dd0ace020acce2bbbe8f3c
Dominique Unruh <unruh@ut.ee>
2020-08-31 01:46:06 +0300

    Initial draft for libisabelle replacement

---
+++ isabelle-thys/control_isabelle.ml
@@ -0,0 +1,82 @@
+structure Control_Isabelle : sig
+  val sendReply : int -> int list -> unit
+  val handleLines : unit -> unit
+  exception E_ExnExn of exn -> exn
+  exception E_Int of int
+  val store : int -> exn -> unit
+end
+=
+struct
+val inStream = TextIO.openIn inputPipeName
+val outStream = TextIO.openOut outputPipeName
+
+val objectsMax = Unsynchronized.ref 0
+val objects : exn Inttab.table Unsynchronized.ref = Unsynchronized.ref Inttab.empty
+
+fun sendReply int ints = let
+  val _ = OS.Process.sleep (seconds 1.0)
+  val str = (String.concatWith " " (map string_of_int (int::ints)) ^ "\n")
+  val _ = tracing ("sendReply: "^str)
+  val _ = TextIO.output (outStream, str)
+  val _ = TextIO.flushOut outStream
+  in () end
+
+exception E_ExnExn of exn -> exn
+exception E_Int of int
+
+fun executeML ml =
+  ML_Compiler.eval (ML_Compiler.verbose true ML_Compiler.flags) Position.none (ML_Lex.tokenize ml)
+
+fun executeMLInt seq ml =
+  executeML ("Control_Isabelle.sendReply "^string_of_int seq^" [" ^ ml ^ "]")
+
+fun store seq exn = let
+  val idx = !objectsMax
+  val _ = objects := Inttab.update_new (idx, exn) (!objects)
+  val _ = objectsMax := idx + 1
+  in sendReply seq [idx] end
+
+fun storeMLExnExn seq ml =
+  executeML ("let open Control_Isabelle val result = E_ExnExn ("^ml^") in store "^string_of_int seq^" result end")
+
+fun retrieveInt seq id = case Inttab.lookup (!objects) id of
+  NONE => error ("no object " ^ string_of_int id)
+  | SOME (E_Int i) => sendReply seq [i]
+  | SOME _ => error ("expected E_Int, got different exn")
+
+fun applyFunc seq f x = case (Inttab.lookup (!objects) f, Inttab.lookup (!objects) x) of
+  (NONE,_) => error ("no object " ^ string_of_int f)
+  | (_,NONE) => error ("no object " ^ string_of_int x)
+  | (SOME (E_ExnExn f), SOME x) => store seq (f x)
+  | _ => error ("object " ^ string_of_int f ^ " is not an E_ExnExn")
+
+fun executeML' ml =
+  executeML ("local open Control_Isabelle in " ^ ml ^ " end")
+
+fun handleLine seq line = case String.sub (line, 0) of
+    (* Mxxx - executes ML code xxx *)
+    #"M" => executeML' (String.extract (line, 1, NONE))
+    (* ixxx - executes ML expression xxx (of type int) and gives response 'seq result' *)
+  | #"i" => executeMLInt seq (String.extract (line, 1, NONE))
+    (* snnn - Parses nnn as integer, stores result as object, response 'seq object#' *)
+  | #"s" => store seq (E_Int (the (Int.fromString (String.extract (line, 1, NONE)))))
+    (* fxxx - Parses xxx as ML function of type exn \<rightarrow> exn, stores xxx as object #seq *)
+  | #"f" => storeMLExnExn seq (String.extract (line, 1, NONE))
+    (* rID - Parses ID as object# and returns the object, assuming it's (E_Int i), response 'seq i' *)
+  | #"r" => retrieveInt seq (the (Int.fromString (String.extract (line, 1, NONE))))
+    (* af x - Parses f,x as object#, f of type E_ExnExn, computes f x, stores the result, response 'seq ID' *)
+  | #"a" => let val (f,x) = case String.fields (fn c => #" "=c) (String.extract (line, 1, NONE)) |> map (the o Int.fromString)
+                            of [f,x] => (f,x) | _ => raise Match
+        in applyFunc seq f x end
+  | cmd => error ("Unknown command " ^ str cmd)
+
+fun handleLines' number = case TextIO.inputLine inStream of
+    NONE => (TextIO.print "No input\n"; ())
+    | SOME line => (handleLine number line; handleLines' (number+1))
+    ;;
+
+fun handleLines () = handleLines' 0
+
+end;
+
+Control_Isabelle.handleLines ()
---
+++ src/main/scala/qrhl/isabelle/Isabelle2.scala
@@ -0,0 +1,212 @@
+package qrhl.isabelle
+
+import java.io.{BufferedWriter, FileInputStream, FileOutputStream, OutputStreamWriter}
+import java.util.concurrent.{ArrayBlockingQueue, BlockingQueue, ConcurrentHashMap}
+
+import scala.annotation.tailrec
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import scala.io.Source
+import scala.sys.process.Process
+
+/*
+trait MLType
+object MLType {
+  case object Int extends MLType
+  case class Fun[A <: MLType, B <: MLType] extends MLType
+}
+*/
+
+class MLValue[A](val id : Future[Isabelle2.ID]) {
+//  def retrieve()(implicit ec: ExecutionContext) : Future[A] = throw new UnsupportedOperationException(s"Retrieving value of $this")
+  def retrieve()(implicit retriever: MLValue.Retriever[A], isabelle: Isabelle2, ec: ExecutionContext) : Future[A] =
+  retriever.retrieve(this)
+}
+
+object MLValue {
+  abstract class Retriever[A] {
+    def retrieve(value: MLValue[A])(implicit isabelle: Isabelle2, ec: ExecutionContext) : Future[A]
+  }
+  implicit object IntRetriever extends Retriever[Int] {
+    override def retrieve(value: MLValue[Int])(implicit isabelle: Isabelle2, ec: ExecutionContext): Future[Int] =
+      value.id.flatMap(isabelle.retrieveInteger(_).future)
+  }
+
+  def apply(i: Int)(implicit isabelle: Isabelle2) =
+    new MLValue[Int](isabelle.storeInteger(i).future)
+
+  // TODO: Automatically add wrapping and unwrapping of exceptions
+  def compileFunction[A,B](ml: String)(implicit isabelle: Isabelle2) =
+    new MLValue[A => B](isabelle.storeFunction(ml).future)
+
+  def applyFunction[A,B](f: MLValue[A=>B], x:MLValue[A])(implicit isabelle: Isabelle2, ec: ExecutionContext) : MLValue[B] =
+    new MLValue(
+      for (fVal <- f.id;
+           xVal <- x.id;
+           fx <- isabelle.applyFunction(fVal,xVal).future)
+        yield fx
+    )
+}
+
+class Isabelle2 {
+  import Isabelle2._
+
+  private val distributionDir = DistributionDirectory.distributionDirectory
+  private val isabelle = "Isabelle2019-RC4/bin/isabelle"
+  private val logic = "QRHL"
+  private val roots = List("isabelle-thys","isabelle-afp")
+  private val userDir = "isabelle-temp/user/Isabelle2019-RC4/.isabelle"
+  private val mlFile = "isabelle-thys/control_isabelle.ml"
+
+  private val sendQueue : BlockingQueue[(String, String => Unit)] = new ArrayBlockingQueue(1000)
+  private val callbacks : ConcurrentHashMap[Int, String => Unit] = new ConcurrentHashMap()
+
+  private def processQueue() : Unit = {
+    println("Process queue thread started")
+    val stream = new FileOutputStream("/home/unruh/svn/qrhl-tool/isabelle-fifo-in")
+    val writer = new BufferedWriter(new OutputStreamWriter(stream, "ascii"))
+    var count = 0
+
+    @tailrec @inline
+    def drainQueue() : Unit = {
+      val elem = sendQueue.poll()
+      if (elem!=null) {
+        val (line,callback) = elem
+        assert(line.endsWith("\n"))
+        println(s"Writing ${line.trim}")
+        if (callback != null)
+          callbacks.put(count, callback)
+        writer.write(line)
+        count += 1
+        drainQueue()
+      }
+    }
+
+    while (true) {
+      val (line,callback) = sendQueue.take()
+      println(s"Writing! ${line.trim}");
+      if (callback != null)
+        callbacks.put(count, callback)
+      writer.write(line)
+      count += 1
+      drainQueue()
+      println("Flushing.")
+      writer.flush()
+      Thread.sleep(100)
+    }
+  }
+
+  private def parseIsabelle() : Unit = {
+    val output = new FileInputStream("/home/unruh/svn/qrhl-tool/isabelle-fifo-out")
+    Source.fromInputStream(output, "ascii").getLines.foreach { line =>
+      val (seq,content) = line.splitAt(line.indexOf(' ')+1)
+      println(s"Received: [$line]")
+      callbacks.get(seq.trim.toInt) match {
+        case null => println(s"No callback $seq")
+        case callback => callback(content)
+      }
+    }
+  }
+
+  private def startProcess() : Process = {
+    val isabelle = distributionDir.resolve(this.isabelle).toString
+    val mlFile = distributionDir.resolve(this.mlFile).toString
+
+    val userDir = distributionDir.resolve(this.userDir)
+    assert(userDir.endsWith(".isabelle"))
+
+    // TODO: use temporary names, delete later, escape for ML
+    val inputPipeName = "/home/unruh/svn/qrhl-tool/isabelle-fifo-in"
+    val outputPipeName = "/home/unruh/svn/qrhl-tool/isabelle-fifo-out"
+
+    val initInOut = s"""val (inputPipeName,outputPipeName) = ("${inputPipeName}","${outputPipeName}")"""
+
+    val cmd : List[String] = List(isabelle,"process","-l",logic,"-e",initInOut,"-f",mlFile) ++
+      (for (r <- roots; a <- List("-d",distributionDir.resolve(r).toString)) yield a)
+
+    val processBuilder = Process(cmd, distributionDir.toFile, "USER_HOME" -> userDir.getParent.toString)
+
+    val processQueueThread = new Thread("Send to Isabelle") {
+      override def run(): Unit = processQueue() }
+    processQueueThread.setDaemon(true)
+    processQueueThread.start()
+
+    val parseIsabelleThread = new Thread("Read from Isabelle") {
+      override def run(): Unit = parseIsabelle() }
+    parseIsabelleThread.setDaemon(true)
+    parseIsabelleThread.start()
+
+    processBuilder.run()
+  }
+
+  private val process = startProcess()
+
+  def send(str: String, callback: String => Unit = null) : Unit =
+    sendQueue.put((str,callback))
+
+  def computeInteger(ml: String): Promise[Int] = {
+    val promise : Promise[Int] = Promise()
+    assert(!ml.contains('\n'))
+    send(s"i$ml\n", { result => promise.success(result.toInt) })
+    promise
+  }
+
+  def storeFunction(ml : String): Promise[ID] = {
+    val promise : Promise[ID] = Promise()
+    assert(!ml.contains('\n'))
+    send(s"f$ml\n", { result => promise.success(result.toInt) })
+    promise
+  }
+
+  def storeInteger(i: Int): Promise[ID] = {
+    val promise : Promise[ID] = Promise()
+    send(s"s$i\n", { result => promise.success(result.toInt) })
+    promise
+  }
+
+  def retrieveInteger(id: ID): Promise[Int] = {
+    val promise: Promise[Int] = Promise()
+    send(s"r$id\n", { result => promise.success(result.toInt) })
+    promise
+  }
+
+  def applyFunction(f: ID, x: ID): Promise[ID] = {
+    val promise: Promise[ID] = Promise()
+    send(s"a$f $x\n", { result => promise.success(result.toInt) })
+    promise
+  }
+}
+
+object Isabelle2 {
+  type ID = Int
+}
+
+object Test {
+  def await[A](x: Future[A]): A = Await.result(x, Duration.Inf)
+
+  def main(args: Array[String]): Unit = {
+    implicit val ec: ExecutionContext = ExecutionContext.global
+    implicit val isabelle: Isabelle2 = new Isabelle2()
+
+    val int1 = MLValue(1234)
+    val int2 = MLValue(43210000)
+    val f : MLValue[Int => Int => Int] = MLValue.compileFunction("fn (E_Int i) => E_ExnExn (fn (E_Int j) => E_Int (i+j))")
+    val f12 = MLValue.applyFunction(MLValue.applyFunction(f,int1),int2)
+
+    println(await(f12.retrieve()))
+
+    /*   val result = for (id1 <- isabelle.storeInteger(1234).future;
+                         id2 <- isabelle.storeInteger(4321).future;
+                         f <- isabelle.storeFunction("fn (E_Int i) => E_ExnExn (fn (E_Int j) => E_Int (i+j))").future;
+                         f_id1 <- isabelle.applyFunction(f,id1).future;
+                         f_id1_id2 <- isabelle.applyFunction(f_id1,id2).future;
+                         result <- isabelle.retrieveInteger(f_id1_id2).future)
+         yield result
+
+       print(await(result))
+   */
+
+    Thread.sleep(5000)
+  }
+
+}
