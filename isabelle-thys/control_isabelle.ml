0660ec292aecb08a7668883e1474b9d7d944ef5e
Dominique Unruh <unruh@ut.ee>
2020-08-31 23:52:16 +0300

    Moved MLValue back out of Isabelle class. Now MLValue is not type safe -> one can use MLValue with the wrong Isabelle instance. It turned out to be too hard to use as an inner class.

--- isabelle-thys/control_isabelle.ml
+++ isabelle-thys/control_isabelle.ml
@@ -1,9 +1,18 @@
 structure Control_Isabelle : sig
+  (* TODO: Does this need to be exported? *)
   val sendReply : int -> int list -> unit
   val handleLines : unit -> unit
   exception E_ExnExn of exn -> exn
   exception E_Int of int
+  exception E_String of string
+(*   exception E_Context of Proof.context
+  exception E_Theory of theory
+  exception E_Typ of typ
+  exception E_Term of term
+  exception E_Cterm of cterm
+  exception E_Thm of thm *)
   val store : int -> exn -> unit
+  (* For diagnostics. Linear time *)
   val numObjects : unit -> int
 end
 =
@@ -16,23 +25,39 @@ val objects : exn Inttab.table Unsynchronized.ref = Unsynchronized.ref Inttab.em
 
 fun numObjects () = Inttab.fold (fn _ => fn i => i+1) (!objects) 0
 
-fun sendReply int ints = let
+fun sendReplyStr seq str = let
   (* val _ = OS.Process.sleep (seconds 0.1) *)
-  val str = (String.concatWith " " (map string_of_int (int::ints)) ^ "\n")
+  val str = string_of_int seq ^ " " ^ str
   val _ = tracing ("sendReply: "^str)
   val _ = TextIO.output (outStream, str)
   val _ = TextIO.flushOut outStream
   in () end
 
+fun sendReply seq ints = let
+  (* val _ = OS.Process.sleep (seconds 0.1) *)
+  val str = (String.concatWith " " (map string_of_int (seq::ints)) ^ "\n")
+  val _ = tracing ("sendReply: "^str)
+  val _ = TextIO.output (outStream, str)
+  val _ = TextIO.flushOut outStream
+  in sendReplyStr seq str end
+
 exception E_ExnExn of exn -> exn
 exception E_Int of int
+exception E_Unit
+exception E_String of string
+(* exception E_Context of Proof.context
+exception E_Theory of theory
+exception E_Typ of typ
+exception E_Term of term
+exception E_Cterm of cterm
+exception E_Thm of thm *)
 
 fun executeML ml =
   ML_Compiler.eval (ML_Compiler.verbose true ML_Compiler.flags) Position.none (ML_Lex.tokenize ml)
 
-fun executeMLInt seq ml = let
+(* fun executeMLInt seq ml = let
   val _ = tracing ("executeMLInt "^string_of_int seq ^" : " ^ml)
-  in  executeML ("Control_Isabelle.sendReply "^string_of_int seq^" [" ^ ml ^ "]") end
+  in  executeML ("Control_Isabelle.sendReply "^string_of_int seq^" [" ^ ml ^ "]") end *)
 
 fun store seq exn = let
   val idx = !objectsMax
@@ -48,14 +73,20 @@ fun retrieveInt seq id = case Inttab.lookup (!objects) id of
   | SOME (E_Int i) => sendReply seq [i]
   | SOME _ => error ("expected E_Int, got different exn")
 
+fun retrieveString seq id = case Inttab.lookup (!objects) id of
+  NONE => error ("no object " ^ string_of_int id)
+  | SOME (E_String str) => sendReplyStr seq str
+  | SOME _ => error ("expected E_String, got different exn")
+
+
 fun applyFunc seq f x = case (Inttab.lookup (!objects) f, Inttab.lookup (!objects) x) of
   (NONE,_) => error ("no object " ^ string_of_int f)
   | (_,NONE) => error ("no object " ^ string_of_int x)
   | (SOME (E_ExnExn f), SOME x) => store seq (f x)
   | _ => error ("object " ^ string_of_int f ^ " is not an E_ExnExn")
 
-fun executeML' ml =
-  executeML ("local open Control_Isabelle in " ^ ml ^ " end")
+(* fun executeML' ml =
+  executeML ("local open Control_Isabelle in " ^ ml ^ " end") *)
 
 fun removeObjects ids =
   objects := fold Inttab.delete ids (!objects)
@@ -67,21 +98,34 @@ fun int_of_string str = case Int.fromString str of
 fun handleLine seq line = (tracing ("COMMAND:"^line);
   case String.sub (line, 0) of
     (* Mxxx - executes ML code xxx *)
-    #"M" => executeML' (String.extract (line, 1, NONE))
+    #"M" => (executeML (String.extract (line, 1, NONE)); sendReply seq [])
+
     (* ixxx - executes ML expression xxx (of type int) and gives response 'seq result' *)
-  | #"i" => executeMLInt seq (String.extract (line, 1, NONE))
+    (*   | #"i" => executeMLInt seq (String.extract (line, 1, NONE)) *)
+
+    (* Sxxx - stores xxx as a string in objects, response 'seq ID' *)
+  | #"S" => store seq (E_String (String.extract (line, 1, NONE)))
+
     (* snnn - Parses nnn as integer, stores result as object, response 'seq object#' *)
   | #"s" => store seq (E_Int (int_of_string (String.extract (line, 1, NONE))))
+
     (* fxxx - Parses xxx as ML function of type exn \<rightarrow> exn, stores xxx as object #seq *)
   | #"f" => storeMLExnExn seq (String.extract (line, 1, NONE))
+
     (* rID - Parses ID as object# and returns the object, assuming it's (E_Int i), response 'seq i' *)
   | #"r" => retrieveInt seq (int_of_string (String.extract (line, 1, NONE)))
+
+    (* RID - Parses ID as object# and returns the object, assuming it's (E_String s), response 'seq s' *)
+  | #"R" => retrieveString seq (int_of_string (String.extract (line, 1, NONE)))
+
     (* af x - Parses f,x as object#, f of type E_ExnExn, computes f x, stores the result, response 'seq ID' *)
   | #"a" => let val (f,x) = case String.fields (fn c => #" "=c) (String.extract (line, 1, NONE)) |> map int_of_string
                             of [f,x] => (f,x) | _ => raise Match
         in applyFunc seq f x end
+
     (* gi j k ... - removes object i,j,k,... from objects *)
   | #"g" => removeObjects (String.extract (line, 1, NONE) |> String.fields (fn c => #" "=c) |> map int_of_string)
+
   | cmd => error ("Unknown command " ^ str cmd))
 
 fun handleLines' number = case TextIO.inputLine inStream of
--- src/main/scala/isabelle/control/Isabelle.scala
+++ src/main/scala/isabelle/control/Isabelle.scala
@@ -154,95 +154,130 @@ class Isabelle {
   private def send(str: String, callback: String => Unit = null) : Unit =
     sendQueue.put((str,callback))
 
-  @deprecated("Remove", "now")
-  def computeInteger(ml: String): Promise[Int] = {
+/*  def computeInteger(ml: String): Promise[Int] = {
     val promise : Promise[Int] = Promise()
     assert(!ml.contains('\n'))
     send(s"i$ml\n", { result => promise.success(result.toInt) })
     promise
-  }
+  }*/
 
   private def intStringToID(str: String) : ID =
     new ID(str.toInt, this)
 
-  private def storeFunction(ml : String): Promise[ID] = {
+  def executeMLCode(ml : String) : Future[Unit] = {
+    val promise : Promise[Unit] = Promise()
+    assert(!ml.contains('\n'))
+    send(s"M$ml\n", { result => promise.success(()) })
+    promise.future
+  }
+
+  def executeMLCodeNow(ml : String): Unit = Await.result(executeMLCode(ml), Duration.Inf)
+
+  private[control] def storeFunction(ml : String): Promise[ID] = {
     val promise : Promise[ID] = Promise()
     assert(!ml.contains('\n'))
     send(s"f$ml\n", { result => promise.success(intStringToID(result)) })
     promise
   }
 
-  private def storeInteger(i: Int): Promise[ID] = {
+  private[control] def storeInteger(i: Int): Promise[ID] = {
     val promise : Promise[ID] = Promise()
     send(s"s$i\n", { result => promise.success(intStringToID(result)) })
     promise
   }
 
-  private def retrieveInteger(id: ID): Promise[Int] = {
-    val promise: Promise[Int] = Promise()
-    send(s"r${id.id}\n", { result => promise.success(result.toInt) })
+  private[control] def storeString(str: String): Promise[ID] = {
+    val promise : Promise[ID] = Promise()
+    assert(!str.contains('\n'))
+    send(s"S$str\n", { result => promise.success(intStringToID(result)) })
     promise
   }
 
-  private def applyFunction(f: ID, x: ID): Promise[ID] = {
+  private[control] def applyFunction(f: ID, x: ID): Promise[ID] = {
     val promise: Promise[ID] = Promise()
     send(s"a${f.id} ${x.id}\n", { result => promise.success(intStringToID(result)) })
     promise
   }
 
-  class MLValue[A] private[isabelle] (private val id : Future[Isabelle.ID]) {
-//    final def isabelle : Isabelle = Isabelle.this
-
-    def retrieve()(implicit retriever: MLValue.Retriever[A], ec: ExecutionContext): Future[A] =
-      retriever.retrieve(this)
-
-    def apply[D, R](arg: MLValue[D])
-                   (implicit ev: MLValue[A] =:= MLValue[D => R], ec: ExecutionContext): MLValue[R] = {
-      new MLValue(
-        for (fVal <- ev(this).id;
-             xVal <- arg.id;
-             fx <- applyFunction(fVal, xVal).future)
-          yield fx
-      )
-    }
-
-    def apply[D1, D2, R](arg1: MLValue[D1], arg2: MLValue[D2])
-                        (implicit ev: MLValue[A] =:= MLValue[D1 => D2 => R], ec: ExecutionContext): MLValue[R] =
-      ev(this).apply[D1, D2 => R](arg1).apply[D2, R](arg2)
-
-    def apply[D1, D2, D3, R](arg1: MLValue[D1], arg2: MLValue[D2], arg3: MLValue[D3])
-                            (implicit ev: MLValue[A] =:= MLValue[D1 => D2 => D3 => R], ec: ExecutionContext): MLValue[R] =
-      ev(this).apply[D1, D2 => D3 => R](arg1).apply[D2, D3 => R](arg2).apply[D3, R](arg3)
+  private[control] def retrieveInteger(id: ID): Promise[Int] = {
+    val promise: Promise[Int] = Promise()
+    send(s"r${id.id}\n", { result => promise.success(result.toInt) })
+    promise
   }
 
-  object MLValue {
-    abstract class Retriever[A] {
-      protected[Isabelle] def retrieve(value: MLValue[A])(implicit ec: ExecutionContext) : Future[A]
-    }
-
-    implicit object IntRetriever extends Retriever[Int] {
-      override protected[Isabelle] def retrieve(value: MLValue[Int])(implicit ec: ExecutionContext): Future[Int] =
-        value.id.flatMap(retrieveInteger(_).future)
-    }
-
-    def apply(i: Int) : MLValue[Int] =
-      new MLValue(storeInteger(i).future)
-
-    // TODO: Automatically add wrapping and unwrapping of exceptions
-    def compileFunction[A,B](ml: String) : MLValue[A => B] =
-      new MLValue(storeFunction(ml).future)
+  private[control] def retrieveString(id: ID): Promise[String] = {
+    val promise: Promise[String] = Promise()
+    send(s"R${id.id}\n", { result => promise.success(result.stripSuffix("\n")) })
+    promise
   }
 
 }
 
 object Isabelle {
-  private final class ID(val id: Int, isabelle: Isabelle) {
+  private[control] final class ID(val id: Int, isabelle: Isabelle) {
     isabelle.cleaner.register(this, new IDCleaner(id, isabelle))
   }
   private final class IDCleaner(id: Int, isabelle: Isabelle) extends Runnable {
     def run(): Unit = isabelle.garbageQueue.add(id)
   }
 }
+
+class MLValue[A] private[isabelle] (private val id : Future[Isabelle.ID]) {
+//  @inline val isabelle : Isabelle.this.type = Isabelle.this
+
+  @inline def retrieve()(implicit retriever: MLValue.Retriever[A], isabelle: Isabelle, ec: ExecutionContext): Future[A] =
+    retriever.retrieve(this)
+
+  @inline def retrieveNow()(implicit retriever: MLValue.Retriever[A], isabelle: Isabelle, ec: ExecutionContext): A =
+    Await.result(retrieve(), Duration.Inf)
+
+  def apply[D, R](arg: MLValue[D])
+                 (implicit ev: MLValue[A] =:= MLValue[D => R], isabelle: Isabelle, ec: ExecutionContext): MLValue[R] = {
+    new MLValue(
+      for (fVal <- ev(this).id;
+           xVal <- arg.id;
+           fx <- isabelle.applyFunction(fVal, xVal).future)
+        yield fx
+    )
+  }
+
+  def apply[D1, D2, R](arg1: MLValue[D1], arg2: MLValue[D2])
+                      (implicit ev: MLValue[A] =:= MLValue[D1 => D2 => R], isabelle: Isabelle, ec: ExecutionContext): MLValue[R] =
+    ev(this).apply[D1, D2 => R](arg1).apply[D2, R](arg2)
+
+  def apply[D1, D2, D3, R](arg1: MLValue[D1], arg2: MLValue[D2], arg3: MLValue[D3])
+                          (implicit ev: MLValue[A] =:= MLValue[D1 => D2 => D3 => R], isabelle: Isabelle, ec: ExecutionContext): MLValue[R] =
+    ev(this).apply[D1, D2 => D3 => R](arg1).apply[D2, D3 => R](arg2).apply[D3, R](arg3)
+}
+
+object MLValue {
+  abstract class Retriever[A] {
+    protected[MLValue] def retrieve(value: MLValue[A])(implicit isabelle: Isabelle, ec: ExecutionContext) : Future[A]
+  }
+
+  implicit object IntRetriever extends Retriever[Int] {
+    override protected[MLValue] def retrieve(value: MLValue[Int])
+                                             (implicit isabelle: Isabelle, ec: ExecutionContext): Future[Int] =
+      value.id.flatMap(isabelle.retrieveInteger(_).future)
+  }
+
+  implicit object StringRetriever extends Retriever[String] {
+    override protected[MLValue] def retrieve(value: MLValue[String])
+                                             (implicit isabelle: Isabelle, ec: ExecutionContext): Future[String] =
+      value.id.flatMap(isabelle.retrieveString(_).future)
+  }
+
+  def apply(i: Int)(implicit isabelle: Isabelle) : MLValue[Int] =
+    new MLValue(isabelle.storeInteger(i).future)
+
+  def apply(str: String)(implicit isabelle: Isabelle) : MLValue[String] =
+    new MLValue(isabelle.storeString(str).future)
+
+  // TODO: Automatically add wrapping and unwrapping of exceptions
+  def compileFunction[A,B](ml: String)(implicit isabelle: Isabelle) : MLValue[A => B] =
+    new MLValue(isabelle.storeFunction(ml).future)
+}
+
 
 object Test {
   def await[A](x: Future[A]): A = Await.result(x, Duration.Inf)
@@ -252,40 +287,23 @@ object Test {
 
     implicit val ec: ExecutionContext = ExecutionContext.global
     implicit val isabelle: Isabelle = new Isabelle()
-    import isabelle.MLValue.IntRetriever
-    type MLValue[A] = isabelle.MLValue[A]
-    val MLValue = isabelle.MLValue
+    import MLValue.{IntRetriever, StringRetriever}
 
     implicit val ev1: MLValue[Int=>Int] =:= MLValue[Int=>Int] = =:=.tpEquals
+    implicit val ev3: MLValue[Int=>String] =:= MLValue[Int=>String] = =:=.tpEquals
     implicit val ev2: MLValue[Int=>Int=>Int] =:= MLValue[Int=>Int=>Int] = =:=.tpEquals
 
-    isabelle.computeInteger("1").future.onComplete(println)
-
-    val int1 = MLValue(1234)
-    val int2 = MLValue(43210000)
-    val add : MLValue[Int => Int => Int] = MLValue.compileFunction("fn (E_Int i) => E_ExnExn (fn (E_Int j) => E_Int (i+j))")
-    val sum = add(int1,int2)
-
-    println(await(sum.retrieve))
-
-    for (i <- 1 to 10000) {
-//      Thread.sleep(100)
-//      System.gc()
-      MLValue(i)
-    }
-    System.gc()
-
-    Thread.sleep(1000)
-    println("GC")
-    System.gc()
-    Thread.sleep(1000)
 //    isabelle.computeInteger("1").future.onComplete(println)
-    println(s"#objects: ${await(isabelle.computeInteger("Control_Isabelle.numObjects()").future)}")
-    Thread.sleep(1000)
-    println(s"#objects: ${await(isabelle.computeInteger("Control_Isabelle.numObjects()").future)}")
 
-    Thread.sleep(1000)
-    println("Exiting")
+    val testStr = "hello"
+    val str1 = MLValue(testStr)
+    val strBack = str1.retrieveNow
+    assert(strBack==testStr)
+
+    val string_of_int : MLValue[Int => String] = MLValue.compileFunction("fn (E_Int i) => E_String (string_of_int i)")
+    val int1 = MLValue(123)
+    val str2 = string_of_int(int1)
+    assert(str2.retrieveNow == "123")
 
   }
 
