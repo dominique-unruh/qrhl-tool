bbba4f5a94d937edb7eafaf66d23f81bb0044dd4
Dominique Unruh <unruh@ut.ee>
2020-11-26 01:49:39 +0200

    Reimplemented HashedPromise.getHash, HashedPromise.get using AtomicReference-state.

--- src/main/scala/hashedcomputation/Hash.scala
+++ src/main/scala/hashedcomputation/Hash.scala
@@ -3,16 +3,17 @@ package hashedcomputation
 import java.nio.ByteBuffer
 import java.security.MessageDigest
 import java.util
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.HOURS
+import java.util.concurrent.atomic.AtomicReference
 
 import com.google.common.cache
-import com.google.common.util.concurrent.Callables
 import hashedcomputation.Fingerprint.Entry
+import hashedcomputation.HashedPromise.State
 import org.apache.commons.codec.binary.Hex
 import org.jetbrains.annotations.{NotNull, Nullable}
 import org.log4s
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -93,99 +94,157 @@ object HashedFunction {
   }
 }
 
-class HashedPromise[A <: HashedValue, B <: HashedValue] {
-  /*
-   * At least one of the following must be defined (non-null):
-   * - hashedFunction & computationInput
-   * - value
-   */
-  /** Only switches from null to non-null. Never changes once non-null */
-  @Nullable private var hash: Hash = _
-  /** Only switches from non-null to null, never changes otherwise */
-  @Nullable private var hashedFunction: HashedFunction[A,B] = _
-  /** Only switches from non-null to null, never changes otherwise */
-  @Nullable private var computationInput: HashedPromise[_,A] = _
-//  /** Only switches from non-null to null, never changes otherwise */
-//  @Nullable private var computationHash: Hash = _
-  /** Only switches from null to non-null. Never changes once non-null */
-  @Nullable private var value: B = _
-
-  // TODO: Make sure the computation is never executed twice!
-  def getHash: Future[Hash] =
-    if (hash!=null) Future.successful(hash)
-    else if (value!=null) Future.successful(value.hash)
-    else
-      for (input <- computationInput.get;
-           hash <- getHashUsingInput(input))
-        yield hash
-
-
-  // TODO: Make sure the computation is never executed twice!
-  def get: Future[B] = synchronized {
-    if (value != null) return Future.successful(value)
-    if (hash != null) Cache.getByHash(hash) match {
-      case Some(hv) => return Future.successful(hv.asInstanceOf[B])
-      case None =>
-    }
-
-    for (input <- computationInput.get;
-         result <- getUsingInput(input))
-      yield result
-  }
-
-  private def getHashUsingInput(input: A): Future[Hash] = synchronized {
-    if (hashedFunction == null) return Future.successful(hash)
-
-    Cache.getHashByInput(hashedFunction.hash, input) match {
-      case Some(hash) => return Future.successful(hash)
-      case None =>
-    }
-
-    for (value <- compute(input))
-      yield { hash = value.hash; hash }
-  }
-
-  private def compute(input: A): Future[B] =
-    for ((result, fingerprint) <- hashedFunction.compute(input);
-         _ = registerResult(result, input, fingerprint))
-      yield result
-
-  private def getUsingInput(input: A): Future[B] = synchronized {
-    if (hashedFunction == null) return Future.successful(value)
-
-    Cache.getByInput(hashedFunction.hash, input) match {
-      case None =>
-      case Some(result) => return Future.successful(result.asInstanceOf[B])
-    }
-
-    compute(input)
-  }
-
-  private def registerResult(result: B, input: A, fingerprint: Fingerprint[A]): Unit = synchronized {
-    val hashedFunctionHash = hashedFunction.hash
-    computationInput = null
-    hashedFunction = null
-    value = result
-    Cache.register(result, hashedFunctionHash, fingerprint)
-  }
-}
-
 object HashedPromise {
-  def apply[A <: HashedValue](hashedValue: A): HashedPromise[HashedValue, A] = {
-    val promise = new HashedPromise[HashedValue, A]
-    promise.value = hashedValue
-    promise
-  }
+  private def apply[A <: HashedValue, B <: HashedValue](state: State[A,B]) =
+    new HashedPromise[A,B](new AtomicReference[State[A, B]](state))
 
-  def apply[A <: HashedValue, B <: HashedValue](hashedFunction: HashedFunction[A, B], input: HashedPromise[_, A]): HashedPromise[A, B] = {
-    val promise = new HashedPromise[A, B]
-    promise.hashedFunction = hashedFunction
-    promise.computationInput = input
-    promise
-  }
+  /** Note: does not add hashedValue to the cache! */
+  def apply[A <: HashedValue](hashedValue: A): HashedPromise[HashedValue, A] =
+    apply(State.Result[HashedValue,A](hashedValue))
+
+  def apply[A <: HashedValue, B <: HashedValue](function: HashedFunction[A, B], input: HashedPromise[_ <: HashedValue, A]): HashedPromise[A, B] =
+    apply(State.FunctionOnly(function, input))
+
+  def apply[A <: HashedValue, B <: HashedValue](function: HashedFunction[A, B], input: HashedPromise[_ <: HashedValue, A], hash: Hash): HashedPromise[A, B] =
+    apply(State.FunctionAndHash(function, input, hash))
 
   def apply[A <: HashedValue, B <: HashedValue](hashedFunction: HashedFunction[A, B], input: A): HashedPromise[A, B] =
     apply(hashedFunction, HashedPromise(input))
+
+  private sealed trait State[A, B]
+  private object State {
+    /** A state in which a computation function is available (but was not executed yet, nor the inputs computed) */
+    sealed trait StateWithFunction[A <: HashedValue, B <: HashedValue] extends State[A,B] {
+      val function : HashedFunction[A,B]
+      val inputFuture: Future[A]
+    }
+    /** A state where a future with the hash is available (hash computation started or finished) */
+    sealed trait HashFutureState[A,B] extends State[A,B] {
+      def hashFuture : Future[Hash]
+    }
+    /** A state where a future with the result is available (hash computation started or finished) */
+    sealed trait ResultFutureState[A,B <: HashedValue] extends HashFutureState[A,B] {
+      def resultFuture : Future[B]
+      override def hashFuture: Future[Hash] = resultFuture.map(_.hash)
+    }
+    /** A state where all computations have been performed (but possibly failed) */
+    sealed trait FinalState[A <: HashedValue, B <: HashedValue] extends HashFutureState[A,B] with ResultFutureState[A,B]
+    sealed trait Computing[A <: HashedValue, B <: HashedValue] extends State[A,B]
+
+    final case class HashAndInput[A <: HashedValue, B <: HashedValue](function: HashedFunction[A,B], input: A, hash: Hash) extends HashFutureState[A,B] {
+      override def hashFuture: Future[Hash] = Future.successful(hash)
+    }
+    final case class Failed[A <: HashedValue, B <: HashedValue](exception: Throwable) extends FinalState[A,B] {
+      override def hashFuture: Future[Hash] = Future.failed(exception)
+      override def resultFuture: Future[B] = Future.failed(exception)
+    }
+    final case class Locked[A <: HashedValue, B <: HashedValue]() extends State[A,B]
+    final case class ComputingHash[A <: HashedValue, B <: HashedValue](override val hashFuture: Future[Hash]) extends HashFutureState[A,B] with Computing[A,B]
+    final case class ComputingResult[A <: HashedValue, B <: HashedValue](override val resultFuture: Future[B]) extends ResultFutureState[A,B] with Computing[A,B]
+    final case class Result[A <: HashedValue, B <: HashedValue](result: B) extends FinalState[A,B] {
+      override def hashFuture: Future[Hash] = Future.successful(result.hash)
+      override def resultFuture: Future[B] = Future.successful(result)
+    }
+    final case class FunctionOnly[A <: HashedValue, B <: HashedValue](override val function: HashedFunction[A, B], input: HashedPromise[_ <: HashedValue, A]) extends StateWithFunction[A,B] {
+      override val inputFuture: Future[A] = input.get
+    }
+    final case class FunctionAndHash[A <: HashedValue, B <: HashedValue]
+          (override val function: HashedFunction[A, B], val input: HashedPromise[_ <: HashedValue, A],
+           hash: Hash)
+      extends StateWithFunction[A,B] with HashFutureState[A,B] {
+      override def hashFuture: Future[Hash] = Future.successful(hash)
+      override val inputFuture: Future[A] = input.get
+    }
+  }
+}
+
+
+
+class HashedPromise[A <: HashedValue, B <: HashedValue](private val state: AtomicReference[HashedPromise.State[A,B]]) extends AnyVal {
+//  private val state: AtomicReference[State[A, B]] = new AtomicReference(initialState)
+
+  private def doCompute(function: HashedFunction[A,B], input: A) : Future[State.FinalState[A,B]] = {
+    val future = for ((result, fingerprint) <- function.compute(input);
+                      _ = Cache.register(result, function.hash, fingerprint))
+      yield State.Result[A,B](result)
+    future.recover { exception => State.Failed[A,B](exception) }
+  }
+
+  /** Guarantees that state is a HashFutureState from now on */
+  @tailrec
+  private def triggerHashComputation(): Unit = {
+    val currentState = state.get()
+    currentState match {
+      case _ : State.HashFutureState[A,B] =>
+      case _ : State.Locked[A,B] =>
+        Thread.sleep(1)
+        triggerHashComputation()
+      case st : State.StateWithFunction[A,B] =>
+        if (state.compareAndSet(st, State.Locked())) {
+          val future = for (inputValue <- st.inputFuture;
+                            function = st.function;
+                            // TODO: We should first try whether we can do the lookup with only the input-hash. Then we might not have to compute the input
+                            hashOption = Cache.getHashByInput(function.hash, inputValue);
+                            newState <- hashOption match {
+                              case Some(hash) => Future.successful(State.HashAndInput(function, inputValue, hash))
+                              case None => doCompute(function, inputValue)
+                            };
+                            _ = state.set(newState);
+                            hash <- newState.hashFuture)
+            yield hash
+          state.set(State.ComputingHash[A,B](future))
+        } else
+          triggerHashComputation()
+    }
+  }
+
+  /** Guarantees that stat is a ResultFutureState from now on (unless it's a Computing state, in which case triggerComputation needs to be invoked again
+   * when the computation is finished). */
+  @tailrec
+  private def triggerComputation(): Unit = {
+    val currentState = state.get()
+    currentState match {
+      case _ : State.Computing[A,B] =>
+      case _ : State.ResultFutureState[A,B] =>
+      case _ : State.Locked[A,B] =>
+        Thread.sleep(1)
+        triggerComputation()
+      case st: State.HashAndInput[A, B] =>
+        ???
+      case st: State.StateWithFunction[A, B] =>
+        if (state.compareAndSet(st, State.Locked())) {
+          val future = for (inputValue <- st.inputFuture;
+                            function = st.function;
+                            resultOption = Cache.getByInput(function.hash, inputValue).asInstanceOf[Option[B]];
+                            newState <- resultOption match {
+                              case Some(result) => Future.successful(State.Result[A, B](result))
+                              case None => doCompute(function, inputValue)
+                            };
+                            _ = state.set(newState);
+                            result <- newState.resultFuture)
+            yield result
+          state.set(State.ComputingResult[A, B](future))
+        } else
+          triggerComputation()
+    }
+  }
+
+
+  def getHash: Future[Hash] = state.get() match {
+    case st: State.HashFutureState[A, B] => st.hashFuture
+    case _ =>
+      triggerHashComputation()
+      getHash
+  }
+
+  def get: Future[B] = state.get() match {
+    case st: State.ResultFutureState[A, B] => st.resultFuture
+    case st: State.ComputingHash[A, B] =>
+      for (_ <- st.hashFuture; result <- get) yield result
+    case _ =>
+      triggerComputation()
+      get
+  }
 }
 
 object Cache {
@@ -210,6 +269,7 @@ object Cache {
     }
   }
 
+  // TODO: Also add getHashByInputHash
   def getHashByInput[A <: HashedValue](@NotNull computationHash: Hash, @NotNull input: A): Option[Hash] = fingerprints.synchronized {
     logger.debug(s"Searching for $computationHash($input) in fingerprints")
     for ((computationHash2, fingerprint, result) <- fingerprints) {
