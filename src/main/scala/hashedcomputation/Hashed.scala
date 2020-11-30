3a6fb2757cc089bfe36ab0855a644300a11af0e3
Dominique Unruh <unruh@ut.ee>
2020-11-30 17:01:25 +0200

    Removed old caching framework

--- src/main/scala/hashedcomputation/Hashed.scala
+++
@@ -1,9 +0,0 @@
-package hashedcomputation
-
-
-case class Hashed[T, Hash](value: T, hash: Hash)
-object Hashed {
-  object Value {
-    def unapply[T,Hash](arg: Hashed[T, Hash]): Some[T] = Some(arg.value)
-  }
-}
