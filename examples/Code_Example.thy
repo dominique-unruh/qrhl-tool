ab6d4bf222d5a688d069dde7f9f777abc223a71d
Dominique Unruh <unruh@ut.ee>
2021-05-04 23:32:52 +0300

    Fixed the examples after bounded operator update

--- examples/Code_Example.thy
+++ examples/Code_Example.thy
@@ -4,25 +4,25 @@ begin
 
 lemma 
   assumes[simp]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
-  shows "Span {EPR} \<guillemotright> \<lbrakk>q1,q2\<rbrakk> \<le> Span {ket (0,0), ket (1,1)} \<guillemotright> \<lbrakk>q1,q2\<rbrakk>"
+  shows "ccspan {EPR} \<guillemotright> \<lbrakk>q1,q2\<rbrakk> \<le> ccspan {ket (0,0), ket (1,1)} \<guillemotright> \<lbrakk>q1,q2\<rbrakk>"
   apply (simp add: prepare_for_code)
   by eval
 
 lemma 
   assumes[simp]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
-  shows "Span {EPR} \<guillemotright> \<lbrakk>q1,q2\<rbrakk> \<le> Span {ket (0,0), ket (1,1)} \<guillemotright> \<lbrakk>q2,q1\<rbrakk>"
+  shows "ccspan {EPR} \<guillemotright> \<lbrakk>q1,q2\<rbrakk> \<le> ccspan {ket (0,0), ket (1,1)} \<guillemotright> \<lbrakk>q2,q1\<rbrakk>"
   apply (simp add: prepare_for_code)
   by eval
 
 lemma 
   assumes[simp]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
-  shows "Span {ket (0::bit,0::bit)} \<guillemotright> \<lbrakk>q1,q2\<rbrakk> \<le> Span {ket (0)} \<guillemotright> \<lbrakk>q1\<rbrakk>"
+  shows "ccspan {ket (0::bit,0::bit)} \<guillemotright> \<lbrakk>q1,q2\<rbrakk> \<le> ccspan {ket (0)} \<guillemotright> \<lbrakk>q1\<rbrakk>"
   apply (simp add: prepare_for_code)
   by eval
 
 lemma
   assumes[simp]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
-  shows "CNOT\<guillemotright>\<lbrakk>q1,q2\<rbrakk> \<cdot> hadamard\<guillemotright>\<lbrakk>q1\<rbrakk> \<cdot> Span {ket (0,0)}\<guillemotright>\<lbrakk>q1,q2\<rbrakk> \<le> Span {EPR}\<guillemotright>\<lbrakk>q1,q2\<rbrakk>"
+  shows "CNOT\<guillemotright>\<lbrakk>q1,q2\<rbrakk> \<cdot> hadamard\<guillemotright>\<lbrakk>q1\<rbrakk> \<cdot> ccspan {ket (0,0)}\<guillemotright>\<lbrakk>q1,q2\<rbrakk> \<le> ccspan {EPR}\<guillemotright>\<lbrakk>q1,q2\<rbrakk>"
   apply (simp add: prepare_for_code)
   by eval
 
--- examples/EPR.thy
+++ examples/EPR.thy
@@ -4,13 +4,13 @@ begin
 
 lemma joint_measure_aux:
   assumes [simp]: "declared_qvars \<lbrakk>q1,r1,q2,r2\<rbrakk>"
-  shows "\<forall>(a::bit) (b::bit). \<lbrakk>q1, r1\<rbrakk> \<equiv>\<qq> \<lbrakk>q2, r2\<rbrakk> \<le> Span {ket (a, b)}\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<sqinter> Span {ket (a, b)}\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<squnion> (- Span {ket (a, b)})\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<squnion> (- Span {ket (a, b)})\<guillemotright>\<lbrakk>q2, r2\<rbrakk>"
+  shows "\<forall>(a::bit) (b::bit). \<lbrakk>q1, r1\<rbrakk> \<equiv>\<qq> \<lbrakk>q2, r2\<rbrakk> \<le> ccspan {ket (a, b)}\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<sqinter> ccspan {ket (a, b)}\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<squnion> (- ccspan {ket (a, b)})\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<squnion> (- ccspan {ket (a, b)})\<guillemotright>\<lbrakk>q2, r2\<rbrakk>"
   apply (simp add: prepare_for_code)
   by eval
 
 lemma final_goal:
   assumes [simp]: "declared_qvars \<lbrakk>q1,r1,q2,r2\<rbrakk>"
-    shows "Span {EPR}\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<sqinter> Span {EPR}\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<le> hadamard\<guillemotright>\<lbrakk>r2\<rbrakk> \<cdot> (hadamard\<guillemotright>\<lbrakk>q1\<rbrakk> \<cdot> \<lbrakk>q1, r1\<rbrakk> \<equiv>\<qq> \<lbrakk>q2, r2\<rbrakk>)"
+    shows "ccspan {EPR}\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<sqinter> ccspan {EPR}\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<le> hadamard\<guillemotright>\<lbrakk>r2\<rbrakk> \<cdot> (hadamard\<guillemotright>\<lbrakk>q1\<rbrakk> \<cdot> \<lbrakk>q1, r1\<rbrakk> \<equiv>\<qq> \<lbrakk>q2, r2\<rbrakk>)"
   apply (simp add: prepare_for_code)
   by eval
 
