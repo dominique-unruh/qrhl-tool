56ab6614dd86ef2fdcd048887e124cf59feab8e0
Dominique Unruh <unruh@ut.ee>
2021-11-10 23:26:31 +0200

    Hiding notation and constants from Infinite_Set_Sum.

--- isabelle-thys/QRHL.thy
+++ isabelle-thys/QRHL.thy
@@ -3,6 +3,10 @@ theory QRHL
     Joint_Measure Joint_Sample Squash_Sampling Universe_Instances_Bounded_Operators O2H Semi_Classical_Search
 begin
 
+no_notation Infinite_Set_Sum.abs_summable_on (infix "abs'_summable'_on" 50)
+hide_const (open) Infinite_Set_Sum.abs_summable_on
+notation Infinite_Sum.abs_summable_on (infixr "abs'_summable'_on" 46)
+
 unbundle notation_norm
 unbundle cblinfun_notation
 unbundle no_jnf_notation
