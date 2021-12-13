6ba5f71f054685f66d11a00c00867ae25d00f44d
Dominique Unruh <unruh@ut.ee>
2021-12-13 17:13:34 +0200

    Some comments edited

--- isabelle-thys/Misc_Missing.thy
+++ isabelle-thys/Misc_Missing.thy
@@ -88,7 +88,8 @@ derive universe bit
   by (metis (full_types) Rep_bit_inverse one_bit.abs_eq zero_bit.abs_eq) *)
 (* lemma bit_two[simp]: "(2::bit) = 0" (* bit_numeral_even *)
   by (metis add_cancel_left_right bit.exhaust one_add_one)  *)
-(* TODO: This rule makes the simplifier loop in Isabelle2021-1-RC0. Check if that's still the case. If not, add [simp] and remove the special cases below. *)
+
+(* This rule makes the simplifier loop in Isabelle2021-1. Check if that's still the case. If not, add [simp] and remove the special cases below. *)
 lemma bit_eq_x: "((a=x) = (b=x)) = (a=b)" for a b x :: bit
   apply transfer by auto
 (* These rules are a replacement for bit_eq_x[simp] in several important cases *)
@@ -107,7 +108,7 @@ lemma bit_eq_x_iff1[simp]: \<open>(((a=x) = (b=x)) \<longleftrightarrow> P) \<lo
 lemma bit_eq_x_iff2[simp]: \<open>(P \<longleftrightarrow> ((a=x) = (b=x))) \<longleftrightarrow> (P \<longleftrightarrow> (a=b))\<close> for a b x :: bit
   apply transfer by auto
 
-(* TODO remove *)
+(* To check whether we loop if we add [simp] to bit_eq_x *)
 lemma \<open>a + b + b = a\<close> for a b :: bit
   apply simp?
   oops
