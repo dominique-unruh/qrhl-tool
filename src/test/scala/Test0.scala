c2eae11dd2625e337fc4216deb1e07575897d683
Dominique Unruh <unruh@ut.ee>
2021-12-28 23:03:45 +0100

    Fixed IsabelleX.theContext. (Sometimes contained failed contexts.)

--- src/test/scala/Test0.scala
+++ src/test/scala/Test0.scala
@@ -1,9 +1,11 @@
+import org.scalatest.funsuite.AnyFunSuite
 import qrhl.isabellex.IsabelleX
 
 import scala.collection.JavaConverters._
 import scala.collection.immutable.ListSet
-
 import qrhl.Utils.ListSetUtils
+import qrhl.isabellex.IsabelleX.globalIsabelle.intT
+import qrhl.toplevel.{DeclareVariableCommand, ToplevelTest}
 
 object Test0 {
   def main(args: Array[String]): Unit = {
@@ -14,4 +16,21 @@ object Test0 {
     println(l)
     println(ListSet.empty +++ l2)
   }
+}
+
+class Test0 extends AnyFunSuite {
+/*
+  test("tmp") {
+    val tl = ToplevelTest.makeToplevelWithTheory()
+    try {
+      tl.execCmd("quantum var rho : program_state")
+    } catch {
+      case e: Throwable => println(e)
+    }
+    println("\n\n\n\n\n RETRY ***********\n\n\n")
+//    val state2 = tl.state.declareAmbientVariable("x", intT)
+//    tl.execCmd("ambient var x : int")
+    tl.execCmd(DeclareVariableCommand("x", intT, true, false))
+  }
+*/
 }
