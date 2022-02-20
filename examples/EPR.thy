03267178da3efcbc14759df79405321140af5595
Dominique Unruh <unruh@ut.ee>
2022-02-20 20:39:09 +0200

    Updated example files for non-overloaded syntax

--- examples/EPR.thy
+++ examples/EPR.thy
@@ -4,13 +4,13 @@ begin
 
 lemma joint_measure_aux:
   assumes [simp]: "declared_qvars \<lbrakk>q1,r1,q2,r2\<rbrakk>"
-  shows "\<forall>(a::bit) (b::bit). \<lbrakk>q1, r1\<rbrakk> \<equiv>\<qq> \<lbrakk>q2, r2\<rbrakk> \<le> ccspan {ket (a, b)}\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<sqinter> ccspan {ket (a, b)}\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<squnion> (- ccspan {ket (a, b)})\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<squnion> (- ccspan {ket (a, b)})\<guillemotright>\<lbrakk>q2, r2\<rbrakk>"
+  shows \<open>\<forall>(a::bit) (b::bit). \<lbrakk>q1, r1\<rbrakk> \<equiv>\<qq> \<lbrakk>q2, r2\<rbrakk> \<le> \<lbrakk>q1, r1\<rbrakk> =\<^sub>\<qq> |(a, b)\<rangle> \<sqinter> \<lbrakk>q2, r2\<rbrakk> =\<^sub>\<qq> |(a, b)\<rangle> \<squnion> \<lbrakk>q1, r1\<rbrakk> \<in>\<^sub>\<qq> (- ccspan { |(a, b)\<rangle> }) \<squnion> \<lbrakk>q2, r2\<rbrakk> \<in>\<^sub>\<qq> (- ccspan { |(a, b)\<rangle> })\<close>
   apply (simp add: prepare_for_code)
   by eval
 
 lemma final_goal:
   assumes [simp]: "declared_qvars \<lbrakk>q1,r1,q2,r2\<rbrakk>"
-    shows "ccspan {EPR}\<guillemotright>\<lbrakk>q2, r2\<rbrakk> \<sqinter> ccspan {EPR}\<guillemotright>\<lbrakk>q1, r1\<rbrakk> \<le> hadamard\<guillemotright>\<lbrakk>r2\<rbrakk> \<cdot> (hadamard\<guillemotright>\<lbrakk>q1\<rbrakk> \<cdot> \<lbrakk>q1, r1\<rbrakk> \<equiv>\<qq> \<lbrakk>q2, r2\<rbrakk>)"
+  shows \<open>\<lbrakk>q2, r2\<rbrakk> =\<^sub>\<qq> EPR \<sqinter> \<lbrakk>q1, r1\<rbrakk> =\<^sub>\<qq> EPR \<le> hadamard\<guillemotright>\<lbrakk>r2\<rbrakk> *\<^sub>S hadamard\<guillemotright>\<lbrakk>q1\<rbrakk> *\<^sub>S \<lbrakk>q1, r1\<rbrakk> \<equiv>\<qq> \<lbrakk>q2, r2\<rbrakk>\<close>
   apply (simp add: prepare_for_code)
   by eval
 
