7231d0a771371629880b3b8c3e514626d2f4d97b
Dominique Unruh <unruh@ut.ee>
2022-02-22 17:05:07 +0200

    Fixed wrong hash creation in State.Fixed wrong hash creation in State. (Lead to changed Isabelle theories sometimes not being reloaded. Maybe other problems, too.)
    Fixes #33.

--- src/test/scala/Test0.scala
+++ src/test/scala/Test0.scala
@@ -1,14 +1,16 @@
 import hashedcomputation.filesystem.{FingerprintedDirectorySnapshot, OutdatedSnapshotException, RootsDirectory}
 import org.scalatest.funsuite.AnyFunSuite
+import qrhl.UserException
 import qrhl.isabellex.IsabelleX
 
 import scala.collection.JavaConverters._
 import scala.collection.immutable.ListSet
 import qrhl.Utils.ListSetUtils
 import qrhl.isabellex.IsabelleX.globalIsabelle.intT
-import qrhl.toplevel.{DeclareVariableCommand, ToplevelTest}
+import qrhl.toplevel.ToplevelTest.emptyState
+import qrhl.toplevel.{DeclareVariableCommand, IncludeCommand, Toplevel, ToplevelTest}
 
-import java.nio.file.Paths
+import java.nio.file.{Files, Paths}
 
 object Test0 {
   def main(args: Array[String]): Unit = {
@@ -35,17 +37,57 @@ object Test0 {
 
 class Test0 extends AnyFunSuite {
 /*
-  test("tmp") {
-    val tl = ToplevelTest.makeToplevelWithTheory()
-    try {
-      tl.execCmd("quantum var rho : program_state")
-    } catch {
-      case e: Throwable => println(e)
+  test ("tmp") {
+    val examples = Paths.get("examples")
+    val thy = examples.resolve("TMP.thy")
+    Files.writeString(thy,
+    """theory TMP
+      |  imports Main
+      |begin
+      |
+      |lemma testlemma1: "1=1" by simp
+      |lemma testlemma2: True by simp
+      |lemmas testlemma = testlemma1
+      |
+      |end
+      |""".stripMargin)
+    val include = examples.resolve("include.qrhl")
+    Files.writeString(include, "isabelle TMP.\n")
+//    Files.deleteIfExists(include)
+//    Files.writeString(include, "")
+    val tl = Toplevel.makeToplevelFromState(emptyState, errorWhenUnfinished = false)
+
+//    tl.execCmd(IncludeCommand("include.qrhl"))
+    tl.execCmd("""include "include.qrhl"""")
+    println(s"###### CONTEXT: ${System.identityHashCode(tl.state.isabelle.context)}")
+    tl.execCmd("lemma True")
+    println(s"###### CONTEXT: ${System.identityHashCode(tl.state.isabelle.context)}")
+
+    println("================ RULE TESTLEMMA #1 ==============")
+    assertThrows[UserException] {
+      tl.execCmd("rule testlemma")
     }
-    println("\n\n\n\n\n RETRY ***********\n\n\n")
-//    val state2 = tl.state.declareAmbientVariable("x", intT)
-//    tl.execCmd("ambient var x : int")
-    tl.execCmd(DeclareVariableCommand("x", intT, true, false))
+    println(s"###### CONTEXT: ${System.identityHashCode(tl.state.isabelle.context)}")
+
+    Files.writeString(thy,
+      """theory TMP
+        |  imports Main
+        |begin
+        |
+        |lemma testlemma1: "1=1" by simp
+        |lemma testlemma2: True by simp
+        |lemmas testlemma = testlemma2
+        |
+        |end
+        |""".stripMargin)
+    Thread.sleep(1000)
+    println("================ RULE TESTLEMMA #2 ==============")
+    println(s"###### CONTEXT: ${System.identityHashCode(tl.state.isabelle.context)}")
+    tl.execCmd("print refl")
+    println(s"###### CONTEXT: ${System.identityHashCode(tl.state.isabelle.context)}")
+    tl.execCmd("rule testlemma")
+    println(s"###### CONTEXT: ${System.identityHashCode(tl.state.isabelle.context)}")
+
   }
 */
 }
