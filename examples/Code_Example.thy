5f17ee3849367598497988c9e06d2e1cb648d473
Dominique Unruh <unruh@ut.ee>
2022-12-04 01:17:34 +0800

    Made `Code_Examples.thy` work.

--- examples/Code_Example.thy
+++ examples/Code_Example.thy
@@ -3,25 +3,25 @@ theory Code_Example
 begin
 
 lemma 
-  assumes[simp]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
+  assumes [register]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
   shows \<open>\<lbrakk>q1, q2\<rbrakk> =\<^sub>\<qq> EPR  \<le>  \<lbrakk>q1, q2\<rbrakk> \<in>\<^sub>\<qq> ccspan { |(0, 0)\<rangle>, |(1, 1)\<rangle> }\<close>
   apply prepare_for_code
   by eval
 
 lemma 
-  assumes[simp]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
+  assumes [register]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
   shows \<open>\<lbrakk>q1, q2\<rbrakk> =\<^sub>\<qq> EPR  \<le>  \<lbrakk>q2, q1\<rbrakk> \<in>\<^sub>\<qq> ccspan { |(0, 0)\<rangle>, |(1, 1)\<rangle> }\<close>
   apply prepare_for_code
   by eval
 
 lemma 
-  assumes[simp]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
+  assumes [register]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
   shows \<open>\<lbrakk>q1, q2\<rbrakk> =\<^sub>\<qq> |(0::bit, 0::bit)\<rangle>  \<le>  \<lbrakk>q1\<rbrakk> =\<^sub>\<qq> |0\<rangle>\<close>
   apply prepare_for_code
   by eval
 
 lemma
-  assumes[simp]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
+  assumes [register]: "declared_qvars \<lbrakk>q1,q2\<rbrakk>"
   shows \<open>(CNOT\<guillemotright>\<lbrakk>q1, q2\<rbrakk> o\<^sub>C\<^sub>L hadamard\<guillemotright>\<lbrakk>q1\<rbrakk>)  *\<^sub>S  \<lbrakk>q1, q2\<rbrakk> =\<^sub>\<qq> |(0, 0)\<rangle>  \<le>  \<lbrakk>q1, q2\<rbrakk> =\<^sub>\<qq> EPR\<close>
   apply prepare_for_code
   by eval
