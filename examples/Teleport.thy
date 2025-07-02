08bb82fd1905f31723e8206d745c2727b0b4dbc9
Dominique Unruh <9913676+dominique-unruh@users.noreply.github.com>
2025-07-02 17:41:55 +0200

    Fixed teleport.qrhl.

--- examples/Teleport.thy
+++ examples/Teleport.thy
@@ -11,21 +11,18 @@ lemma assoc_op_lift_aux:
   using assms by (metis (no_types, lifting) V_def double_adj distinct_qvars_split2 distinct_qvars_swap
       qvar_trafo_adj qvar_trafo_assoc_op qvar_trafo_l2bounded)
 
-lemma assoc_replace: 
-  assumes "A o\<^sub>C\<^sub>L B = C"
-  shows "D o\<^sub>C\<^sub>L A o\<^sub>C\<^sub>L B = D o\<^sub>C\<^sub>L C"
-  by (simp add: cblinfun_compose_assoc assms) 
-
 lemma teleport_goal1:
   assumes[simp]: "declared_qvars \<lbrakk>A1,B1,C1,A2\<rbrakk>"
   shows \<open>quantum_equality_full (addState EPR*) \<lbrakk>C1, A1, B1\<rbrakk> id_cblinfun \<lbrakk>A2\<rbrakk> \<le> CNOT\<guillemotright>\<lbrakk>C1, A1\<rbrakk> *\<^sub>S hadamard\<guillemotright>\<lbrakk>C1\<rbrakk> *\<^sub>S quantum_equality_full (addState EPR* o\<^sub>C\<^sub>L (assoc_op* o\<^sub>C\<^sub>L (CNOT \<otimes>\<^sub>o id_cblinfun o\<^sub>C\<^sub>L (assoc_op o\<^sub>C\<^sub>L hadamard \<otimes>\<^sub>o id_cblinfun)))) \<lbrakk>C1, A1, B1\<rbrakk> id_cblinfun \<lbrakk>A2\<rbrakk>\<close>
 proof -
-  have hadamard: "(hadamard \<otimes> id_cblinfun) o\<^sub>C\<^sub>L (hadamard \<otimes>\<^sub>o id_cblinfun) = id_cblinfun" by simp
-  have CNOT: "(CNOT \<otimes>\<^sub>o id_cblinfun) o\<^sub>C\<^sub>L (CNOT \<otimes>\<^sub>o id_cblinfun) = id_cblinfun" by simp
+  have hadamard: "(hadamard \<otimes>\<^sub>o id_cblinfun) o\<^sub>C\<^sub>L (hadamard \<otimes>\<^sub>o id_cblinfun) = id_cblinfun"
+    by (simp add: comp_tensor_op)
+  have CNOT: "(CNOT \<otimes>\<^sub>o id_cblinfun) o\<^sub>C\<^sub>L (CNOT \<otimes>\<^sub>o id_cblinfun) = id_cblinfun"
+    by (simp add: comp_tensor_op)
   show ?thesis
     by (simp add: distinct_qvars_split1 distinct_qvars_split2 cblinfun_compose_assoc[symmetric] assoc_op_lift_aux
         lift_extendR[where U=hadamard and R="\<lbrakk>A1,B1\<rbrakk>"] lift_extendR[where U=CNOT and R="\<lbrakk>B1\<rbrakk>"]
-        assoc_replace[OF hadamard] assoc_replace[OF unitaryD2] assoc_replace[OF CNOT] assoc_replace[OF isometryD])
+        lift_cblinfun_comp[OF hadamard] lift_cblinfun_comp[OF unitaryD2] lift_cblinfun_comp[OF CNOT] lift_cblinfun_comp[OF isometryD])
 qed
 
 lemma teleport_goal2_a0c0:
--- isabelle-thys/BOLegacy.thy
+++ isabelle-thys/BOLegacy.thy
@@ -123,4 +123,145 @@ definition remove_qvar_unit_op :: "(('a*unit) ell2,'a ell2) cblinfun" where
 
 abbreviation \<open>addState \<equiv> tensor_ell2_right\<close>
 
+
+lemma basis_enum_of_vec_tensor_ell2:
+  \<open>(basis_enum_of_vec x :: 'a ell2) \<otimes>\<^sub>l (basis_enum_of_vec y :: 'b ell2) = basis_enum_of_vec (tensor_state_jnf x y)\<close>
+  if \<open>dim_vec x = CARD('a::enum)\<close> and \<open>dim_vec y = CARD('b::enum)\<close> for x y
+  using that
+  by (metis (full_types) basis_enum_of_vec_inverse canonical_basis_length_ell2
+      vec_of_basis_enum_inverse vec_of_basis_enum_tensor_state)
+
+
+lemma tensor_ccsubspace_code[code]:
+ "tensor_ccsubspace (SPAN A :: 'a::enum ell2 ccsubspace) (SPAN B :: 'b::enum ell2 ccsubspace)
+    = SPAN [tensor_state_jnf a b. a<-filter (\<lambda>v. dim_vec v = CARD('a)) A, b<-filter (\<lambda>v. dim_vec v = CARD('b)) B]"
+proof -
+  define AA BB where \<open>AA = filter (\<lambda>v. dim_vec v = CARD('a)) A\<close> and \<open>BB = filter (\<lambda>v. dim_vec v = CARD('b)) B\<close>
+
+  have \<open>tensor_ccsubspace (SPAN A :: 'a::enum ell2 ccsubspace) (SPAN B :: 'b::enum ell2 ccsubspace)
+    = ccspan (basis_enum_of_vec ` set AA) \<otimes>\<^sub>S ccspan (basis_enum_of_vec ` set BB)\<close>
+    by (simp add: SPAN_def AA_def BB_def filter_set)
+  also have \<open>\<dots> = ccspan {x \<otimes>\<^sub>l y |x y. x \<in> basis_enum_of_vec ` set AA \<and> y \<in> basis_enum_of_vec ` set BB}\<close>
+    by (simp add: tensor_ccsubspace_ccspan)
+  also have \<open>\<dots> = ccspan {basis_enum_of_vec x \<otimes>\<^sub>l basis_enum_of_vec y |x y. x \<in> set AA \<and> y \<in> set BB}\<close>
+    apply (rule arg_cong[where f=ccspan])
+    by blast
+  also have \<open>\<dots> = ccspan {basis_enum_of_vec (tensor_state_jnf x y) |x y. x \<in> set AA \<and> y \<in> set BB}\<close>
+    apply (rule arg_cong[where f=ccspan])
+    using basis_enum_of_vec_tensor_ell2
+    by (force intro!: simp: AA_def BB_def)
+  also have \<open>\<dots> = ccspan (basis_enum_of_vec ` (\<Union>x\<in>set AA. tensor_state_jnf x ` set BB))\<close>
+    apply (rule arg_cong[where f=ccspan])
+    by auto
+  also have \<open>\<dots> = ccspan (basis_enum_of_vec `
+      Set.filter (\<lambda>v. dim_vec v = CARD('a) * CARD('b)) (\<Union>x\<in>set AA. tensor_state_jnf x ` set BB))\<close>
+    by (auto intro!: arg_cong[where f=ccspan] arg_cong[where f=\<open>image _\<close>] Set_filter_unchanged[symmetric] simp: AA_def BB_def)
+  also have \<open>\<dots> = SPAN [tensor_state_jnf a b. a<-AA, b<-BB]\<close>
+    by (simp add: SPAN_def)
+  finally show ?thesis
+    by (simp add: AA_def BB_def)
+qed
+
+
+lemma mat_of_cblinfun_tensor_ell2_right_index:
+  includes jnf_syntax and no vec_syntax
+  fixes \<psi> :: \<open>'a::enum ell2\<close>
+  assumes [simp]: \<open>i < CARD('b) * CARD('a)\<close>
+  assumes [simp]: \<open>j < CARD('b)\<close>
+  shows \<open>mat_of_cblinfun (tensor_ell2_right \<psi> :: ('b::enum ell2 \<Rightarrow>\<^sub>C\<^sub>L _)) $$ (i,j) = 
+            (let (i1,i2) = tensor_unpack CARD('b) CARD('a) i in
+                  of_bool (i1=j) * vec_of_basis_enum \<psi> $ i2)\<close>
+proof -
+  define i1 i2
+    where "i1 = fst (tensor_unpack CARD('b) CARD('a) i)"
+      and "i2 = snd (tensor_unpack CARD('b) CARD('a) i)"
+  have [simp]: "i1 < CARD('b)" "i2 < CARD('a)" 
+    using assms i1_def tensor_unpack_bound1 apply presburger
+    using assms i2_def tensor_unpack_bound2 by blast
+
+  have \<open>mat_of_cblinfun (tensor_ell2_right \<psi> :: ('b::enum ell2 \<Rightarrow>\<^sub>C\<^sub>L _)) $$ (i,j) 
+       = Rep_ell2 ((tensor_ell2_right \<psi> :: ('b::enum ell2 \<Rightarrow>\<^sub>C\<^sub>L _)) *\<^sub>V ket (Enum.enum!j)) (Enum.enum ! i)\<close>
+    by (simp add: mat_of_cblinfun_ell2_component)
+  also have \<open>\<dots> = Rep_ell2 (ket(enum_class.enum ! j :: 'b) \<otimes>\<^sub>l \<psi>) (enum_class.enum ! i1, enum_class.enum ! i2)\<close>
+    by (simp add: tensor_op_ell2 enum_prod_nth_tensor_unpack[where i=i] Let_def case_prod_beta flip: tensor_ell2_ket i1_def i2_def)
+  also have \<open>\<dots> = of_bool ((enum_class.enum ! i1 :: 'b) = enum_class.enum ! j) * (ket (enum_class.enum ! i2) \<bullet>\<^sub>C \<psi>)\<close>
+    by (simp add: cinner_ket flip: cinner_ket_left tensor_ell2_ket)
+  also have \<open>\<dots> = of_bool (i1 = j) * (ket (enum_class.enum ! i2) \<bullet>\<^sub>C \<psi>)\<close>
+    apply (rule arg_cong[where f=\<open>\<lambda>x. of_bool x * _\<close>])
+    apply (rule enum_inj)
+    by simp_all
+  also have \<open>\<dots> = of_bool (i1=j) * vec_of_basis_enum \<psi> $ i2\<close>
+    by (simp add: vec_of_basis_enum_ell2_component cinner_ket_left)
+  finally show ?thesis
+    by (simp add: Let_def i1_def i2_def split!: prod.split)
+qed
+
+lemma mat_of_cblinfun_tensor_ell2_right[code]:
+  includes jnf_syntax and no vec_syntax
+  fixes \<psi> :: \<open>'a::enum ell2\<close>
+  shows \<open>mat_of_cblinfun (tensor_ell2_right \<psi> :: ('b::enum ell2 \<Rightarrow>\<^sub>C\<^sub>L _)) = 
+           tensor_op_jnf (1\<^sub>m CARD('b)) (mat_of_cols (CARD('a)) [vec_of_ell2 \<psi>])\<close>
+    (is \<open>?lhs = ?rhs\<close>)
+proof (rule eq_matI, simp_all add: canonical_basis_length)
+  fix i j assume i_leq: \<open>i < CARD('b) * CARD('a)\<close> and j_leq: \<open>j < CARD('b)\<close>
+  define i1 i2
+    where "i1 = fst (tensor_unpack CARD('b) CARD('a) i)"
+      and "i2 = snd (tensor_unpack CARD('b) CARD('a) i)"
+  then have [simp]: \<open>tensor_unpack CARD('b) CARD('a) i = (i1,i2)\<close>
+    by force
+  have [simp]: \<open>tensor_unpack CARD('b) (Suc 0) j = (j,0)\<close>
+    by (simp add: tensor_unpack_def)
+
+  have i1_leq: "i1 < CARD('b)" and i2_leq: "i2 < CARD('a)" 
+    using i_leq i1_def tensor_unpack_bound1 apply presburger
+    using i_leq i2_def tensor_unpack_bound2 by blast
+
+  have \<open>?lhs $$ (i, j) = of_bool (i1 = j) * vec_of_ell2 \<psi> $ i2\<close>
+    apply (subst mat_of_cblinfun_tensor_ell2_right_index)
+    by (simp_all add: i_leq j_leq vec_of_ell2_def)
+  also have \<open>\<dots> = of_bool (i1=j) * mat_of_cols CARD('a) [vec_of_ell2 \<psi>] $$ (i2, 0)\<close>
+    by (simp add: i2_leq mat_of_cols_Cons_index_0)
+  also have \<open>\<dots> = 1\<^sub>m CARD('b) $$ (i1, j) * mat_of_cols CARD('a) [vec_of_ell2 \<psi>] $$ (i2, 0)\<close>
+    by (simp add: index_one_mat i1_leq j_leq)
+  also have \<open>\<dots> = ?rhs $$ (i, j)\<close>
+    by (simp add: tensor_op_jnf_def case_prod_beta Let_def canonical_basis_length i_leq j_leq)
+  finally show \<open>?lhs $$ (i,j) = ?rhs $$ (i,j)\<close>
+    by -
+qed
+
+lemma conjugate_1[simp]: \<open>conjugate (1 :: 'a :: conjugatable_field) = 1\<close>
+  by (metis conjugate_dist_mul conjugate_id mult.commute verit_prod_simplify(1))
+
+lemma tensor_op_jnf_id_right[simp]: \<open>tensor_op_jnf a (1\<^sub>m 1 :: 'a::{monoid_mult,zero} mat) = a\<close>
+  by (auto simp: tensor_op_jnf_def tensor_unpack_def)
+
+lemma mat_adjoint_one[simp]: \<open>mat_adjoint (1\<^sub>m n) = 1\<^sub>m n\<close>
+    apply (rule eq_matI)
+  by (auto intro!: simp: mat_adjoint_def mat_of_cols_Cons_index_0 mat_of_rows_index)
+
+declare mat_of_cblinfun_tensor_op[code]
+
+
+lemma remove_qvar_unit_op_code[code]: \<open>mat_of_cblinfun (remove_qvar_unit_op :: (_ \<Rightarrow>\<^sub>C\<^sub>L 'a::enum ell2)) = 1\<^sub>m (CARD('a))\<close>
+proof -
+  have *: \<open>mat_of_cols 1 [vec_of_basis_enum (1::unit ell2)] = 1\<^sub>m 1\<close>
+    apply (rule eq_matI)
+    by (auto intro!: simp: vec_of_basis_enum_1 mat_of_cols_Cons_index_0)
+  have \<open>mat_of_cblinfun (remove_qvar_unit_op :: (_ \<Rightarrow>\<^sub>C\<^sub>L 'a::enum ell2))
+    = mat_adjoint (tensor_op_jnf (1\<^sub>m CARD('a)) (1\<^sub>m 1))\<close>
+    using * by (simp add: remove_qvar_unit_op_def mat_of_cblinfun_adj mat_of_cblinfun_tensor_ell2_right vec_of_ell2_def)
+  also have \<open>\<dots> = mat_adjoint (1\<^sub>m CARD('a))\<close>
+    apply (subst tensor_op_jnf_id_right)
+    by simp
+  also have \<open>\<dots> = 1\<^sub>m CARD('a)\<close>
+    by simp
+  finally show ?thesis
+    by -
+qed
+
+(* Something is broken which gets fixed by this. Check occasionally whether Teleport.thy works if this line is removed. If so, this line can be removed again. *)
+declare finite'_code[code del]
+
+declare tensor_op_adjoint[simp]
+
 end
